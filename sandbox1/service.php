3bb09e0a5d0ca7a9f35e4afb0764e15398317983
AgassiWongCG <112854010+AgassiWongCG@users.noreply.github.com>
2023-02-25 14:39:47 +0100

    Update service.php

--- sandbox1/service.php
+++ sandbox1/service.php
@@ -1,3 +1,4 @@
+
 <?php
     session_start();
     require_once "config.php";
@@ -12,6 +13,45 @@
     $req = $bdd->prepare("SELECT * FROM effectif WHERE token = ?");
     $req->execute([$_SESSION["user"]]);
     $data = $req->fetch();
+
+    ($connect = mysqli_connect("localhost", "root", "")) or die("erreur de connection à MySQL");
+    mysqli_select_db($connect, "lsmc") or die("erreur de connexion à la base de données");
+    $idEffectifActuel = $data["id"];
+
+    //                                                       0      1          2              3        4      5       6      7         8       9
+    $result                 = mysqli_query($connect, "SELECT id, effectif, debutservice, finservice, total, dernier, heure, minute, seconde, jour FROM service WHERE effectif=$idEffectifActuel");
+
+    $result_total           = mysqli_query($connect, "SELECT SUM(seconde) AS seconde, SUM(minute) AS minute, SUM(heure) AS heure, SUM(jour) AS jour, dernier FROM service WHERE effectif=$idEffectifActuel AND dernier=0");
+
+    while ($currentService = mysqli_fetch_row($result_total)) {
+        $total_jour     = $currentService[3];
+        $total_heure    = $currentService[2];
+        $total_minute   = $currentService[1];
+        $total_seconde  = $currentService[0];
+
+        $minute_supp = 0;
+        if ($total_seconde > 59) {
+            $minute_supp = intval($total_seconde / 60);
+            $total_seconde = $total_seconde % 60;
+            $total_minute = $total_minute + $minute_supp;
+        }
+
+        $heure_supp = 0;
+        if ($total_minute > 59) {
+            $heure_supp = intval($total_minute / 60);
+            $total_minute = $total_minute % 60;
+            $total_heure = $total_heure + $heure_supp;
+        }
+
+        $jour_supp = 0;
+        if ($total_minute > 23) {
+            $jour_supp = intval($total_heure / 24);
+            $total_heure = $total_heure % 24;
+            $total_jour = $total_jour + $jour_supp;
+        }
+
+
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,10 +59,48 @@
 		<title>Prise de Service</title>
 		<link rel="stylesheet" href="./css/style.css">
 		<style>
+            /* PALETTE COULEURS */
+		    /* https://coolors.co/01161e-124559-598392-aec3b0-eff6e0 */
 		    body {
 		        color: #ffffff;
 		        background-color: #01161e;
 		    }
+		    .selectInputStyle {
+                width: 300px;
+                height: 40px;
+                text-align: center;
+                font-weight: bold;
+                border-radius: 10px;
+                margin-bottom: 20px;
+		    }
+
+            .textInputStyle {
+                width: 300px;
+                height: 40px;
+                text-align: center;
+                font-weight: bold;
+                border-radius: 10px;
+                margin-bottom: 20px;
+            }
+            .statutTitleStyle, .vehicleTitleStyle, .remarksTitleStyle {
+                font-size: 1.5rem;
+                font-weight: bold;
+            }
+            .rowEffectifHeader {
+                background-color: #0F252E;
+            }
+            .rowEffectifPair {
+                background-color: #124559;
+            }
+            .rowEffectifImpair {
+                background-color: #3080A0;
+            }
+            .bold {
+                font-weight: bold;
+            }
+            .titleSummaryStyle {
+                font-weight: bold;
+            }
 		</style>
         <script language="javascript" type="text/javascript">
 
@@ -66,7 +144,6 @@
 			}
 		</script>
 	</head>
-
 	<body style="width: 100%; text-align: center;">
 	<?php
         // Info : Donnée de connexion au serveur PHPMyAdmin
@@ -182,98 +259,196 @@
         $nbrTotalService_LSMC   = mysqli_num_rows(mysqli_query($connect, "SELECT * FROM effectif WHERE service = true AND hospital = 'lsmc'"));
         $nbrTotalService_BCMC   = mysqli_num_rows(mysqli_query($connect, "SELECT * FROM effectif WHERE service = true AND hospital = 'bcmc'"));
     ?>
-    <h4>Prise de service</h4>
-    <form method="post" action="service.php" style="width: 100%; text-align: center;">
-        <table style="width: 100%; text-align: center;">
-            <tbody>
-                <tr>
-                    <td>
-                        Bonjour <?php echo $data["firstname"];?> <?php echo $data["lastname"];?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Satut
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <select name="Statut">
-                            <option value="Code 3">Code 3</option>
-                            <option selected="selected" value="Code 6">Code 6</option>
-                            <option value="Code 7">Code 7</option>
-                            <option value="Code 10">Code 10</option>
-                            <option value="Code 15">Code 15</option>
-                            <option value="Autopsie">Autopsie</option>
-                            <option value="Morgue">Morgue</option>
-                            <option value="Fusillade">Fusillade</option>
-                            <option value="Consultation">Consultation</option>
-                            <option value="Entretien">Entretien</option>
-                            <option value="Formation">Formation</option>
-                            <option value="Examen">Examen</option>
-                            <option value="Rendez-vous">Rendez-vous</option>
-                            <option value="Evenement">Évênement</option>
-                            <option value="Reunion">Réunion</option>
-                            <option value="Gestion Direction">Gestion Direction</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Véhicule utilisé</td>
-                </tr>
-                <tr>
-                    <td>
-                        <select name="Vehicule">
-                            <option selected="selected" value="Aucune">Aucun</option>
-                            <option value="Ambulance 1">Ambulance 1</option>
-                            <option value="Ambulance 2">Ambulance 2</option>
-                            <option value="Vapide">Vapide</option>
-                            <option value="4x4">4x4</option>
-                            <option value="Caracara">Caracara</option>
-                            <option value="Alamo">Alamo</option>
-                            <option value="Predator">Predator</option>
-                            <option value="Buffalo Direction">Buffalo Direction</option>
-                            <option value="Helicoptere">Helicoptere</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Unité | Remarque | Zone
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="text" name="Commentaire"/>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <?php if($data["service"] === 0) echo '<input type="submit" onmousover="errorCheck_PriseDeService()" name="button_PriseDeService" value="Prise de Service" class="btn btn-danger btn-lg"/>'?>
-        <?php if($data["service"] === 1) echo '<input type="submit" onmousover="errorCheck_MiseAJour()" name="button_MiseAJour" value="Mettre à jour" class="btn btn-danger btn-lg"/>'?>
-        <?php if($data["service"] === 1) echo '<input type="submit" onmousover="errorCheck_FinDeService()" name="button_FinDeService" value="Fin de service" class="btn btn-danger btn-lg"/>'?>
-        <a href="landing.php" class="btn btn-danger btn-lg">Retour Profil</a>
-    </form>
+    <table style="width: 100%; text-align: center; margin: 20px 0px 20px 0px;">
+    	<tbody>
+    		<tr>
+    			<td style="width: 25%;">
+    			    <a href="landing.php" class="btn btn-danger btn-lg">Retour Profil</a>
+    			</td>
+    			<td style="width: 50%;">
+    			    <h1>Prise de service</h1>
+    			</td>
+    			<td style="width: 25%;">
+
+    			</td>
+    		</tr>
+    	</tbody>
+    </table>
+
+    <table style="width: 100%;">
+    	<tbody>
+    		<tr>
+    			<td style="width: 33%;">
+                    <table style="width: 100%;">
+                        <tbody>
+                            <tr>
+                                <td style="width: 50%;" class="titleSummaryStyle">Prénom :</td>
+                                <td style="width: 50%;"><?php echo ucfirst($data["firstname"]);?></td>
+                            </tr>
+                            <tr>
+                                <td class="titleSummaryStyle">Nom :</td>
+                                <td><?php echo ucfirst($data["lastname"]);?></td>
+                            </tr>
+                            <tr>
+                                <td class="titleSummaryStyle">Grade :</td>
+                                <td><?php echo ucfirst($data["grade"]);?></td>
+                            </tr>
+                            <tr>
+                                <td class="titleSummaryStyle">Rôle :</td>
+                                <td><?php echo ucfirst($data["role"]);?></td>
+                            </tr>
+                            <tr>
+                                <td class="titleSummaryStyle">Agrégations :</td>
+                                <td><?php echo ucfirst($data["agregation"]);?></td>
+                            </tr>
+                            <tr>
+                                <td class="titleSummaryStyle">Téléphone :</td>
+                                <td><?php echo ucfirst($data["phone"]);?></td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td class="titleSummaryStyle">Temps total de Service Effectué cette semaine:</td>
+                                <td>Jour(s) : <?php echo $total_jour?><br/>Heure(s) : <?php echo $total_heure?><br/>Minute(s) : <?php echo $total_minute?><br/>Seconde(s) : <?php echo $total_seconde?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+    			<td style="width: 34%;">
+                    <form method="post" action="service.php" style="width: 100%; text-align: center;">
+                        <table style="width: 100%; text-align: center;">
+                            <tbody>
+                                <tr>
+                                    <td class="statutTitleStyle">
+                                        Satut
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <select name="Statut" class="selectInputStyle">
+                                            <option value="Code 3">Code 3</option>
+                                            <option selected="selected" value="Code 6">Code 6</option>
+                                            <option value="Code 7">Code 7</option>
+                                            <option value="Code 10">Code 10</option>
+                                            <option value="Code 15">Code 15</option>
+                                            <option value="Autopsie">Autopsie</option>
+                                            <option value="Morgue">Morgue</option>
+                                            <option value="Fusillade">Fusillade</option>
+                                            <option value="Consultation">Consultation</option>
+                                            <option value="Entretien">Entretien</option>
+                                            <option value="Formation">Formation</option>
+                                            <option value="Examen">Examen</option>
+                                            <option value="Rendez-vous">Rendez-vous</option>
+                                            <option value="Evenement">Évênement</option>
+                                            <option value="Reunion">Réunion</option>
+                                            <option value="Gestion Direction">Gestion Direction</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="vehicleTitleStyle">
+                                        &nbsp;
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="vehicleTitleStyle">
+                                        Véhicule utilisé
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <select name="Vehicule" class="selectInputStyle">
+                                            <option selected="selected" value="Aucune">Aucun</option>
+                                            <option value="Ambulance 1">Ambulance 1</option>
+                                            <option value="Ambulance 2">Ambulance 2</option>
+                                            <option value="Vapide">Vapide</option>
+                                            <option value="4x4">4x4</option>
+                                            <option value="Caracara">Caracara</option>
+                                            <option value="Alamo">Alamo</option>
+                                            <option value="Predator">Predator</option>
+                                            <option value="Buffalo Direction">Buffalo Direction</option>
+                                            <option value="Helicoptere">Helicoptere</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="vehicleTitleStyle">
+                                        &nbsp;
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="remarksTitleStyle">
+                                        Unité | Remarques | Zone
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="text" name="Commentaire" class="textInputStyle"/>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <?php if($data["service"] === 0) echo '
+                            <table style="width: 100%;">
+                                <tr>
+                                    <td style="width: 100%;">
+                                        <input type="submit" onmousover="errorCheck_PriseDeService()" name="button_PriseDeService" value="Prise de Service" class="btn btn-danger btn-lg"/>
+                                    </td>
+                                </tr>
+                            </table>
+                        '?>
+                        <?php if($data["service"] === 1) echo '
+                            <table style="width: 100%;">
+                                <tr>
+                                    <td style="width: 50%; text-align: right; padding: 20px;">
+                                        <input type="submit" onmousover="errorCheck_MiseAJour()" name="button_MiseAJour" value="Mettre à jour" class="btn btn-danger btn-lg"/>
+                                    </td>
+                                    <td style="width: 50%; text-align: left; padding: 20px;">
+                                        <input type="submit" onmousover="errorCheck_FinDeService()" name="button_FinDeService" value="Fin de service" class="btn btn-danger btn-lg"/>
+                                    </td>
+                                </tr>
+                            </table>
+                        '?>
+                    </form>
+    			</td>
+    			<td style="width: 33%;">&nbsp;</td>
+    		</tr>
+    	</tbody>
+    </table>
+
 
     <p>Nombre en Service : <?php echo $nbrTotalService ?></p>
     <p>Nombre en Service LSMC : <?php echo $nbrTotalService_LSMC ?></p>
     <p>Nombre en Service BCMC : <?php echo $nbrTotalService_BCMC ?></p>
 
-    <table border="1" cellpadding="2" cellspacing="2">
-                <tr>
-                    <th>Id</th>
-                    <th>Hôpital</th>
-                    <th>Prénom Nom</th>
-                    <th>Grade</th>
-                    <th>Rôle</th>
-                    <th>Agrégations</th>
-                    <th>Téléphone</th>
-                    <th>Intervention</th>
-                    <th>Commentaire</th>
-                    <th>Véhicule</th>
-                    <th>Début du service</th>
-                </tr>
-<?php    while ($row = mysqli_fetch_row($result)) {
+    <table border="1" cellpadding="10px" cellspacing="10px" style="width: 95%; margin: 0px 50px 0px 50px;">
+        <tr class="rowEffectifHeader">
+            <th>Id</th>
+            <th>Hôpital</th>
+            <th>Prénom Nom</th>
+            <th>Grade</th>
+            <th>Rôle</th>
+            <th>Agrégations</th>
+            <th>Téléphone</th>
+            <th>Intervention</th>
+            <th>Commentaire</th>
+            <th>Véhicule</th>
+            <th>Début du service</th>
+        </tr>
+<?php
+    $rank = 0;
+    while ($row = mysqli_fetch_row($result)) {
+        $rowPair = 'rowEffectifPair';
+        $rowImpair = 'rowEffectifImpair';
+        $rowAppliedClass = '';
+        if ($rank % 2 === 0) { // PAIR
+            $rowAppliedClass = $rowPair;
+        }
+        if ($rank % 2 === 1) { // IMPAIR
+            $rowAppliedClass = $rowImpair;
+        }
         $id                 = $row[0];
         $hopital            = $row[1];
         $prenom             = $row[2];
@@ -288,7 +463,17 @@
         $debutservice       = $row[11];
         $service            = $row[12];
 
-    echo "<tr>
+    // Info : Reformattage Textuel
+    $hopital = strtoupper($hopital);
+    $prenom = ucfirst($prenom);
+    $nom = ucfirst($nom);
+    $grade = ucfirst($grade);
+    $role = ucfirst($role);
+    $phone = substr_replace($phone, ' ', 3, 0);
+    $phone = substr_replace($phone, ' ', 6, 0);
+
+    echo "
+        <tr class='$rowAppliedClass'>
             <td>$id</td>
             <td>$hopital</td>
             <td>$prenom $nom</td>
@@ -300,7 +485,9 @@
             <td>$commentaire</td>
             <td>$vehicule</td>
             <td>$debutservice</td>
-        </tr>";
+        </tr>
+    ";
+    $rank += 1;
     }
 ?>
 <?php
